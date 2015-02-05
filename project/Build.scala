0b2eb4fe4938678706ffe48001b8c24d117afee9
rongxie <rxie@juniper.net>
2015-02-05 14:00:14 -0800

    change Build.scala to solve root-subproject issue'

--- project/Build.scala
+++ project/Build.scala
@@ -2,6 +2,8 @@ import sbt._
 import Keys._
 
 object ProjectBuild extends Build {
-  lazy val root = Project(id = "hello", base = file("hello"))
+  lazy val root = Project(id = "myproject", base = file("."))
+    .aggregate(hello) 
+  lazy val hello = Project(id = "hello", base = file("hello"))
 
 } 
